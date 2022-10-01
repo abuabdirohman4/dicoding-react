556b71b5eed75d3f629fba980b710baeeb6ed42d
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-01 23:14:15 +0700

    done hamburger menu

--- submission/app.js
+++ submission/app.js
@@ -1,43 +1,52 @@
-let listSkills = [
-    {
-      name: "HTML",
-      src: "./assets/image/html.svg",
-    },
-    {
-      name: "CSS",
-      src: "./assets/image/css.svg",
-    },
-    {
-      name: "JScript",
-      src: "./assets/image/js.svg",
-    },
-    {
-      name: "React",
-      src: "./assets/image/react.svg",
-    },
-    {
-      name: "Vue",
-      src: "./assets/image/vue.svg",
-    },
-    {
-      name: "Laravel",
-      src: "./assets/image/laravel.png",
-    },
-  ];
-  
-  listSkills.forEach((skill) => {
-    let skillsWrapper = document.querySelector("#skills .wrapper");
-    let img = document.createElement("img");
-    img.src = skill.src;
-    img.alt = skill.name;
-  
-    let p = document.createElement("p");
-    p.appendChild(document.createTextNode(skill.name));
-  
-    let section = document.createElement("section");
-    section.setAttribute("class", "card card-skill");
-    section.appendChild(img);
-    section.appendChild(p);
-  
-    skillsWrapper.appendChild(section);
-  });
+const listSkills = [
+  {
+    name: "HTML",
+    src: "./assets/image/html.svg",
+  },
+  {
+    name: "CSS",
+    src: "./assets/image/css.svg",
+  },
+  {
+    name: "JScript",
+    src: "./assets/image/js.svg",
+  },
+  {
+    name: "React",
+    src: "./assets/image/react.svg",
+  },
+  {
+    name: "Vue",
+    src: "./assets/image/vue.svg",
+  },
+  {
+    name: "Laravel",
+    src: "./assets/image/laravel.png",
+  },
+];
+
+listSkills.forEach((skill) => {
+  const skillsWrapper = document.querySelector("#skills .wrapper");
+  const img = document.createElement("img");
+  img.src = skill.src;
+  img.alt = skill.name;
+
+  const p = document.createElement("p");
+  p.appendChild(document.createTextNode(skill.name));
+
+  const section = document.createElement("section");
+  section.setAttribute("class", "card card-skill");
+  section.appendChild(img);
+  section.appendChild(p);
+
+  skillsWrapper.appendChild(section);
+});
+
+// Hamburger Menu
+const menuToggle = document.querySelector('nav .menu-toggle input')
+const nav = document.querySelector('nav ul')
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('slide')
+    document.querySelector('nav .menu-toggle').classList.toggle('slide')
+})
