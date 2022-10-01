735ffef8975e7720bd79e27192bbd24bb56b24db
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-10-01 15:43:23 +0700

    submission web : about me

--- submission/app.js
+++ submission/app.js
@@ -1,2 +1,43 @@
-import listSkills from "./assets/scripts/listSkills.js";
-import carousel from "./assets/scripts/listSkills.js";
+let listSkills = [
+    {
+      name: "HTML",
+      src: "./assets/image/html.svg",
+    },
+    {
+      name: "CSS",
+      src: "./assets/image/css.svg",
+    },
+    {
+      name: "Javascript",
+      src: "./assets/image/js.svg",
+    },
+    {
+      name: "React",
+      src: "./assets/image/react.svg",
+    },
+    {
+      name: "Vue",
+      src: "./assets/image/vue.svg",
+    },
+    {
+      name: "Laravel",
+      src: "./assets/image/laravel.png",
+    },
+  ];
+  
+  listSkills.forEach((skill) => {
+    let sectionSkills = document.getElementById("sectionSkills");
+  
+    let img = document.createElement("img");
+    img.src = skill.src;
+  
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(skill.name));
+  
+    let div = document.createElement("div");
+    div.setAttribute("class", "card card-skill");
+    div.appendChild(img);
+    div.appendChild(p);
+  
+    sectionSkills.appendChild(div);
+  });
